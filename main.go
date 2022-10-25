59b946086240f7fec7f6a42ccf32cd60d7ca8e31
oxicuza <oxicusa@gmail.com>
2022-10-25 10:51:52 +0700

    project final

--- main.go
+++ main.go
@@ -2,82 +2,239 @@ package main
 
 import (
 	"fmt"
-	"io"
+	"github.com/eclipse/paho.mqtt.golang"
+	"github.com/gin-gonic/gin"
+	mqttServer "github.com/mochi-co/mqtt/server"
+	"github.com/mochi-co/mqtt/server/events"
+	"github.com/mochi-co/mqtt/server/listeners"
+	"github.com/urfave/cli"
+	"log"
 	"net/http"
 	"os"
-	"os/signal"
-	"runtime"
-	"unicode/utf8"
-
-	"github.com/eclipse/paho.mqtt.golang/packets"
-	"github.com/fhmq/hmq/broker"
-	"github.com/fhmq/hmq/logger"
-	"go.uber.org/zap"
 )
 
-var log = logger.Get()
+// Configuration Structure to Hold the Config Values
+type Configuration struct {
+	MqttServerURL string
+	MqttUsername  string
+	MqttPassword  string
+	BasicAuthUser string
+	BasicAuthPass string
+	GinPort       string
+}
+
+// Container Structure to Hold both Config and MQTTClient
+type Container struct {
+	Config     Configuration
+	MqttClient mqtt.Client
+}
+
+// PublishDTO is the sample data transfer object for publish http route
+type PublishDTO struct {
+	Topic   string `binding:"required" json:"topic"`
+	Message string `binding:"required" json:"message"`
+}
+
+type SubsDTO struct {
+	Topic   string `json:"topic"`
+	Message string `json:"message"`
+}
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	config, err := broker.ConfigureConfig(os.Args[1:])
-	if err != nil {
-		log.Error("configure broker config error: ", zap.Error(err))
+
+	app := cli.NewApp()
+	app.Name = "HTTP-MQTT Bridge"
+	app.Usage = "Makes a bridge from HTTP to MQTT"
+	app.Action = func(c *cli.Context) error {
+		cli.ShowAppHelp(c)
+		return nil
 	}
 
-	b, err := broker.NewBroker(config)
-	if err != nil {
-		log.Fatal("New Broker error: ", zap.Error(err))
-	}
-	b.Start()
-	print("[done mqtt]\n")
-
-	http.HandleFunc("/sensor/temperature", func(writer http.ResponseWriter, request *http.Request) {
-		res, err := io.ReadAll(request.Body)
-		if err != nil {
-		}
-		fmt.Printf("host: %v method: %v uri: %v: data: %v", request.Host, request.Method, trimFirstRune(request.URL.Path), string(res))
-		switch request.Method {
-		case "GET":
-
-			break
-		case "POST":
-			b.PublishMessage(&packets.PublishPacket{
-				FixedHeader: packets.FixedHeader{
-					MessageType:     3,
-					Dup:             false,
-					Qos:             2,
-					Retain:          true,
-					RemainingLength: 50,
+	app.Commands = []cli.Command{
+		{
+			Name:    "start",
+			Aliases: []string{"s"},
+			Usage:   "starts the server",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:   "mqtt-host, mh",
+					Value:  "tcp://localhost:1883",
+					Usage:  "MQTT Host Address",
+					EnvVar: "MQTT_HOST",
 				},
-				TopicName: "sensor/temperature",
-				MessageID: 1,
-				Payload:   res,
-			})
-			fmt.Println("Success send HTTP to MQTT")
-			fmt.Fprintf(writer, "Success !")
-			break
-		default:
-			break
-		}
-	})
+				cli.StringFlag{
+					Name:   "mqtt-user,mu",
+					Value:  "user",
+					Usage:  "MQTT Host Username",
+					EnvVar: "MQTT_USER",
+				},
+				cli.StringFlag{
+					Name:   "mqtt-pass,mp",
+					Value:  "password",
+					Usage:  "MQTT Host Password",
+					EnvVar: "MQTT_PASS",
+				},
+				cli.StringFlag{
+					Name:   "username,u",
+					Value:  "try",
+					Usage:  "Basic Authentication Username",
+					EnvVar: "AUTH_USERNAME",
+				},
+				cli.StringFlag{
+					Name:   "password,p",
+					Value:  "catch",
+					Usage:  "Basic Authentication Password",
+					EnvVar: "AUTH_PASSWORD",
+				},
+				cli.StringFlag{
+					Name:   "port",
+					Value:  "8090",
+					Usage:  "GIN Router Port",
+					EnvVar: "PORT",
+				},
+			},
+			Action: func(c *cli.Context) error {
+				// Fills the Configuration
+				container := InitializeContainer(c)
+				// Setup Gin Server + MQTT
+				// Setup(container)
 
-	go http.ListenAndServe(":8090", nil)
-	print("[done http]\n")
+				/**
+				INITIALISASI VARIABLE
+				*/
+				var subFromMQTT = new(SubsDTO)
 
-	s := waitForSignal()
-	fmt.Print("signal received, broker closed.", s)
+				/**
+				MQTT SERVER
+				*/
+				// Create the new MQTT Server.
+				server := mqttServer.NewServer(nil)
+
+				// Create a TCP listener on a standard port.
+				tcp := listeners.NewTCP("t1", ":1883")
+
+				// Add the listener to the server with default options (nil).
+				err := server.AddListener(tcp, nil)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				// Start the broker. Serve() is blocking - see examples folder
+				// for usage ideas.
+				go func() {
+					err = server.Serve()
+					if err != nil {
+						log.Fatal(err)
+					}
+				}()
+
+				// Add OnMessage Event Hook
+				go func() {
+					server.Events.OnProcessMessage = func(cl events.Client, pk events.Packet) (pkx events.Packet, err error) {
+						fmt.Printf("< OnMessage received message from client %s: %s\n", cl.ID, string(pk.Payload))
+
+						subFromMQTT.Topic = pk.TopicName
+						subFromMQTT.Message = string(pk.Payload)
+
+						err = server.Publish(pk.TopicName, []byte(subFromMQTT.Message), false)
+						if err != nil {
+							log.Fatal(err)
+						}
+
+						return pkx, nil
+					}
+
+					// Add OnSubscribe Event Hook
+					server.Events.OnSubscribe = func(filter string, cl events.Client, qos byte) {
+						fmt.Printf("<< OnSubscribe client subscribed %s: %s %v\n", cl.ID, filter, qos)
+					}
+				}()
+
+				/**
+				MQTT CLIENT
+				*/
+				opts := mqtt.NewClientOptions().AddBroker(container.Config.MqttServerURL)
+				if container.Config.MqttUsername != "" {
+					opts.Username = container.Config.MqttUsername
+					opts.Password = container.Config.MqttPassword
+				}
+				client := mqtt.NewClient(opts)
+				if token := client.Connect(); token.Wait() && token.Error() != nil {
+					log.Fatal("MQTT Error: ", token.Error())
+				}
+				container.MqttClient = client
+
+				/**
+				SETUP GIN
+				*/
+				r := gin.Default()
+
+				// Ping Test
+				r.GET("/ping", func(c *gin.Context) {
+					c.String(http.StatusOK, "pong")
+				})
+
+				// Ping Test
+				r.GET("/", func(c *gin.Context) {
+					c.String(http.StatusOK, "i am ready !")
+				})
+
+				subcribe := r.Group("/subscribe")
+				subcribe.GET("/sensor/suhu", func(c *gin.Context) {
+					c.JSON(http.StatusOK, subFromMQTT)
+				})
+
+				authorized := r.Group("/", gin.BasicAuth(gin.Accounts{
+					container.Config.BasicAuthUser: container.Config.BasicAuthPass, // user:foo password:bar
+				}))
+
+				// Post To Topic
+				authorized.POST("publish", func(c *gin.Context) {
+					var publishDTO PublishDTO
+					// Validate Payloadd
+					if err := c.ShouldBindJSON(&publishDTO); err != nil {
+						c.JSON(http.StatusBadRequest, gin.H{"status": "error", "error": err.Error()})
+						return
+					}
+
+					subFromMQTT.Topic = publishDTO.Topic
+					subFromMQTT.Message = publishDTO.Message
+
+					// Go For MQTT Publish
+					client := container.MqttClient
+					if token := client.Publish(publishDTO.Topic, 0, false, publishDTO.Message); token.Error() != nil {
+						// Return Error
+						log.Println("Error:", token.Error())
+						c.JSON(http.StatusFailedDependency, gin.H{"status": "error", "error": token.Error()})
+						return
+					}
+					c.JSON(http.StatusCreated, gin.H{"status": "ok"})
+				})
+
+				r.Run(":" + container.Config.GinPort)
+
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
 
-func waitForSignal() os.Signal {
-	signalChan := make(chan os.Signal, 1)
-	defer close(signalChan)
-	signal.Notify(signalChan, os.Kill, os.Interrupt)
-	s := <-signalChan
-	signal.Stop(signalChan)
-	return s
-}
-
-func trimFirstRune(s string) string {
-	_, i := utf8.DecodeRuneInString(s)
-	return s[i:]
+// InitializeContainer Create the container with provided cli config values
+func InitializeContainer(c *cli.Context) *Container {
+	container := new(Container)
+	container.Config = Configuration{
+		MqttServerURL: c.String("mqtt-host"),
+		MqttUsername:  c.String("mqtt-user"),
+		MqttPassword:  c.String("mqtt-pass"),
+		BasicAuthUser: c.String("username"),
+		BasicAuthPass: c.String("password"),
+		GinPort:       c.String("port"),
+	}
+	return container
 }
--- plugins/bridge/mock.go
+++
@@ -1,7 +0,0 @@
-package bridge
-
-type mockMQ struct{}
-
-func (m *mockMQ) Publish(e *Elements) (bool, error) {
-	return false, nil
-}
